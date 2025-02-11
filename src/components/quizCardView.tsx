17a26d20641cedc7718952138f460afe0928874b
samoleu <samuel.ferralves@gmail.com>
2025-02-11 17:10:48 -0300

    feat: add navigation for quiz details page

--- src/components/quizCardView.tsx
+++ src/components/quizCardView.tsx
@@ -1,16 +1,26 @@
 import Quiz from '../types/quiz';
+import { useRouter } from 'next/navigation';
 
 interface QuizProps { 
   quiz: Quiz
 }
 
 const QuizCardView = ({ quiz } : QuizProps) => {
+  const router = useRouter();
+
+  const handleDetailsClick = () => {
+    router.push(`/quiz-detalhes/${quiz.videoLink}`);
+  };
+
   return (
   <div className="card bg-base-100 w-96 shadow-xl rounded-lg">
     <div className="card-header flex justify-between rounded-t-lg p-4" style={{ backgroundColor: '#3B3B4C' }}>
       <h2 className="card-title text-white text-xl" style={{whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis', display: 'block' }} title={quiz.title}>
         {quiz.title}
       </h2>
+      <div className='link link-hover flex items-center text-xs'>
+        <a onClick={handleDetailsClick} className='link link-hover cursor-pointer'>Detalhes</a>
+      </div>
     </div>
     <div className="card-body p-2">
       <div className="grid grid-cols-10 gap-2">
