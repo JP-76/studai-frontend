9365dfc22dc093f6f539fe78fd8e1a2f8ca0f8f9
samoleu <samuel.ferralves@gmail.com>
2025-02-11 01:11:06 -0300

    feat: create bottom bar with gradient effect

---
+++ src/components/bottomBar.tsx
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+
+const BottomBar = () => {
+  return (
+    <div className="fixed bottom-0 left-0 w-full h-12 bg-gradient-to-t from-[#12121B] to-transparent"/>
+  );
+};
+
+export default BottomBar;
