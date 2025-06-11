cf279ef111fc8a159073ccffb45c5811086649bd
João Pedro Spinassé <jpspinasse@gmail.com>
2025-06-11 09:42:37 -0300

    creating vite project

--- src/components/componentTemplate.tsx
+++
@@ -1,9 +0,0 @@
-const ComponentTemplate = () => {
-  return (
-    <div>
-      <h1>Component Template</h1>
-    </div>
-  );
-};
-
-export default ComponentTemplate;
--- src/types/attempt.ts
+++
@@ -1,8 +0,0 @@
-export default interface Attempt {
-  id: string;
-  quizId: string;
-  userId: string;
-  score: number;
-  completionDate: string;
-  timeSpent: number;
-}
