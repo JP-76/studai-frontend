df6e4ab83e8b8d4bf0197218b2bfc8710dd90e23
Jhonata Demuner <jhonata.demuner@gmail.com>
2025-07-02 22:09:00 -0300

    feat: add axios and hot toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
 import Home from './pages/Home';
+import Auth from './pages/Auth';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </>
   );
 }
 
